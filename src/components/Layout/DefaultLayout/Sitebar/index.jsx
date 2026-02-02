238dcc022f0d3a9d15e2206dcb57f21fce048bbc
Ha An <anaoanh59@gmail.com>
2026-02-02 10:06:34 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sitebar/index.jsx
+++ src/components/Layout/DefaultLayout/Sitebar/index.jsx
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sitebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sitebar() {
-    return <h2>Sitebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sitebar</h2>
+        </aside>
+    );
 }
 
 export default Sitebar;
